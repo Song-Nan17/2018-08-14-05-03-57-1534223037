7fd30d4ae242891c8bed4d75a1bcb419904e2d6d
Song <songnan17@qq.com>
2018-09-23 23:40:50 +0800

    add my code

--- main/main.js
+++ main/main.js
@@ -1,3 +1,10 @@
-module.exports = function main() {
+module.exports = function main(s) {
+ var sum = 0;
+ var x = 0;
+ for (i = 0;i < s.length;i++){
+	 var x = new Number(s[i]);
+	 sum = sum + x;
+  }
+  return sum;
   // Write your code here
 };
